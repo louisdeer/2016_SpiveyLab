7785ec116d3b5d0bfbeefb856dea1e9db19036d9
louisdeer <louisdeer@gmail.com>
2016-10-30 15:07:03 -0700

    PseudoCode I
    Basic stuff

--- 2016_UnderTopTest/New.m
+++ 2016_UnderTopTest/New.m
@@ -6,3 +6,35 @@
 
 
 % PseudoCode : 
+% initialization
+
+% instruction
+    % 1 screen
+
+% Practice rounds
+    % 13 rounds
+    % same as actual test
+    
+% notice the test begins
+    % 1 screens
+    
+% Actual Test
+    % 30 rounds
+    % stimuli
+        % description
+        % Picture
+    % options
+        % T/F
+    % Choose a better picture if false otherwise go on
+        % 4 options
+    % next
+        % click the arrow
+    
+% end
+    % 1 screen
+        % you are done!
+        
+%-----------------------
+% function list:
+% F (Whole Screen covered a  .jpg)
+% F ()
