c8e71aca6ea844a254b46d5c7f03d8394be3f30b
louisdeer <louisdeer@gmail.com>
2016-11-08 21:37:10 -0800

    Conclude for now
    dd

--- 2016_UnderTopTest/myfuncName.m
+++ 2016_UnderTopTest/myfuncName.m
@@ -2,5 +2,11 @@
 function[c, d] = myfuncName(a,b)
 c=a+b
 d=a-b
+
+CSVfile = fopen('Test.txt', 'w');
+
+fprintf(CSVfile, '%s','Cue,/n');
+
+fclose('Test.txt');
 end
 %use [k,s] = myfuncName to get both values
